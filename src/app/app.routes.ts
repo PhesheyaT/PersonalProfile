5299b84acea802db1f0c69705540c314f91ded09
Phesheya <thembalethupk@gmail.com>
2025-08-04 04:28:28 +0200

    Resolving deploy issues

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,7 @@
 import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { SkillAndExperienceComponent } from './pages/skill-and-experience/skill-and-experience.component';
-import { CertificationsComponent } from './pages/certifications/certifications.component';
-import { AboutMeComponent } from './pages/about-me/about-me.component';
 
 export const routes: Routes = [
-  {path: '', component:AboutMeComponent},
-  {path: 'about-me', component:AboutMeComponent},
-  {path: 'skills-and-experience', component:SkillAndExperienceComponent},
-  {path: 'certifications', component:CertificationsComponent}
 ];
 
 @NgModule({
